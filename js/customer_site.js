c143daa821160c8c2c80618153012573be01a273
huangmingxing1212133-ctrl <huangmingxing1212133@gmail.com>
2025-12-13 02:17:44 +0800

    777

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,8 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
         name: 'Bili资源',
+        default: true,        // ← 加上这行 = 默认勾选 + 默认开启搜索
+        isNormal: true        // ← 建议也加这行，让它出现在「普通资源」那一栏
     }
 };
 
